fa155be9f63118ae6f76bb8980feb8c2da5c511a
Joshua Egwuenu <54110458+JOSH-360@users.noreply.github.com>
2020-03-27 11:37:07 +0100

    jS_task1

---
+++ javascript-task.js
@@ -0,0 +1,9 @@
+var myName = "Joshua Egwuenu";
+console.log ("My name is: "+ name);
+
+var courses = ("Html", "CSS", "Javascript", "Design", "and", "Mobile")
+console.log ("My courses are:"+courses)
+
+for (var i = 0; i <= 200; i++) {
+    if (i % 2 !== 0) console.log(i);
+}
